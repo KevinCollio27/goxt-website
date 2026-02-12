5c186276e30f2da49cf6d4eacb7b29269202d836
Bxnj1ta <benjamin.gonza.pa@gmail.com>
2026-02-12 16:24:08 -0300

    Ajuste para evitar merge

--- src/components/ui/ChatWidget.tsx
+++ src/components/ui/ChatWidget.tsx
@@ -1,11 +1,12 @@
 "use client";
+
 import Script from "next/script";
 
 export function ChatWidget() {
     return (
         <Script
-            strategy="lazyOnload"
             src="https://api-crm.goxt.io/api/widget/embed.js"
+            strategy="afterInteractive"
             data-api-key="wk_0ac4fed868da91973c3916dfa0f4a7c4d3a9ead19b97772a"
         />
     );
